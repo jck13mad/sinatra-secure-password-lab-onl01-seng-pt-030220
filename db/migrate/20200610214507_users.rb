5fdcf3edc11d5f2736a139c327a9c45fc94c9e63
Jack Madden <jck13mad@gmail.com>
2020-06-11 02:28:19 +0000

    Automatically backed up by Learn

--- db/migrate/20200610214507_users.rb
+++ db/migrate/20200610214507_users.rb
@@ -1,10 +1,9 @@
+
 class Users < ActiveRecord::Migration[5.1]
-  
   def change
     create_table :users do |t|
-      t.string :user_name 
+      t.string :username
       t.string :password_digest
     end
   end
-  
 end
