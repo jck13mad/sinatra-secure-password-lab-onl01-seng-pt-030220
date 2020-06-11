02840b7f941a99ca9c6ff6cd5a232dd25cb91a95
Jack Madden <jck13mad@gmail.com>
2020-06-11 02:26:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
       redirect to '/failure'
     end
     
-    @user = User.new(:username => params[:username], :password => params[:password])
+    user = User.new(:username => params[:username], :password => params[:password])
 
     if user.save
       redirect "/login"
